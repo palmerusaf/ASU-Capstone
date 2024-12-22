464a61023d815a54f1078e32a669871e0729b316
Branden Palmer <brandenpalmer08@gmail.com>
2024-12-22 17:33:26 +0700

    add main layout

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useMemo, useState } from 'react';
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="flex gap-2 p-2 h-svh">
+      <div className="w-52 bg-gray-300 rounded-lg">side</div>
+      <div className="w-full bg-gray-300 rounded-lg">sdlfj</div>
+    </div>
+  );
 }
 
 export default App;
