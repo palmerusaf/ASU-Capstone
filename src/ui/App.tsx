e5f2c262564eb03c11b9647770c047d002c1549a
Branden Palmer <brandenpalmer08@gmail.com>
2024-12-22 18:52:35 +0700

    add main layout

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,12 +1,41 @@
 import { useEffect, useMemo, useState } from 'react';
 
-function App() {
+export default function App() {
+  type View = 'link' | 'tracker' | 'search';
+  const [activeView, setActiveView] = useState<View>('link');
   return (
     <div className="flex gap-2 p-2 h-svh">
-      <div className="w-52 bg-gray-300 rounded-lg">side</div>
-      <div className="w-full bg-gray-300 rounded-lg">sdlfj</div>
+      <div className="flex flex-col gap-2 p-4 text-center bg-gray-300 rounded-lg text-nowrap w-fit">
+        <div className="text-lg font-bold underline select-none">Menu</div>
+        <Button title="Link Accounts" view="link" />
+        <Button title="Find Jobs" view="search" />
+        <Button title="Job Tracker" view="tracker" />
+      </div>
+      <div className="p-4 w-full bg-gray-300 rounded-lg">
+        <MainView />
+      </div>
     </div>
   );
-}
 
-export default App;
+  function Button({ title, view }: { title: string; view: View }) {
+    return (
+      <button
+        className={`py-2 px-3 bg-gray-400 rounded-full duration-150 hover:scale-105 ${activeView == view && 'font-bold'}`}
+        onClick={() => setActiveView(view)}
+      >
+        {title}
+      </button>
+    );
+  }
+
+  function MainView() {
+    switch (activeView) {
+      case 'tracker':
+        return 'tracker';
+      case 'link':
+        return 'link';
+      case 'search':
+        return 'search';
+    }
+  }
+}
