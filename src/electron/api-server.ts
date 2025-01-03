ec53bca822e72798ad2242e7788270d12ef93ea5
Branden Palmer <brandenpalmer08@gmail.com>
2025-01-03 14:27:39 +0700

    add drizzle

--- src/electron/api-server.ts
+++ src/electron/api-server.ts
@@ -1,4 +1,39 @@
+import { eq } from 'drizzle-orm';
+import { usersTable } from '../db/schema.js';
 import express from 'express';
+import { drizzle } from 'drizzle-orm/libsql';
+
+const db = drizzle('file:local.db');
+const user: typeof usersTable.$inferInsert = {
+  name: 'John',
+  age: 30,
+  email: 'john@example.com',
+};
+
+await db.insert(usersTable).values(user);
+console.log('New user created!');
+
+const users = await db.select().from(usersTable);
+console.log('Getting all users from the database: ', users);
+/*
+  const users: {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  }[]
+  */
+
+await db
+  .update(usersTable)
+  .set({
+    age: 31,
+  })
+  .where(eq(usersTable.email, user.email));
+console.log('User info updated!');
+
+// await db.delete(usersTable).where(eq(usersTable.email, user.email));
+// console.log('User deleted!');
 
 export function getApiUrl(): string {
   return _apiUrl;
