3d0d318e57810ad931caa0d011d9f649f639e9cc
Branden Palmer <brandenpalmer08@gmail.com>
2025-01-05 16:53:47 +0700

    remove boilerplate

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,39 +1,10 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
 import { api } from './main'
 
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
   const q = api.greeting.useQuery({ name: 'World' })
   if (q.isError) return <div>{q.error.message}</div>
   if (q.isLoading) return <div>{'loading'}</div>
-  if (q.data) return <div>{q.data.text}</div>
-  return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
-  )
+  return <div>{q.data.text}</div>
 }
 
 export default App
