ced2a5c6ef2f8e9d125ed57108dec062fb14bc7b
Branden Palmer <brandenpalmer08@gmail.com>
2025-01-08 21:26:32 +0700

    add no connect to job search

--- src/renderer/src/JobPages.tsx
+++ src/renderer/src/JobPages.tsx
@@ -1,46 +1,42 @@
 import {
   AlertDialog,
   AlertDialogAction,
-  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger
+  AlertDialogTitle
 } from '@renderer/components/ui/alert-dialog'
-import { Button } from '@renderer/components/ui/button'
-
 import { api } from './WithProviders'
-import * as icons from 'lucide-react'
 import * as card from './components/ui/card'
 
 export function SearchJobs() {
   const list = api.connect.getList.useQuery()
-  if (list.data?.length) {
-    return (
-      <>
-        <Alert />
-        <div className="flex justify-center items-center py-2">
-          <card.Card className="w-full max-w-2xl">
-            <card.CardTitle>
-              <card.CardHeader>
-                <card.CardTitle className="flex gap-2 justify-center items-center text-xl">
-                  No Providers Connected
-                </card.CardTitle>
-              </card.CardHeader>
-            </card.CardTitle>
-          </card.Card>
-        </div>
-      </>
-    )
-  }
+  if (!list.data?.length) return <NoConnect />
+  return 'no implemento'
 }
 
-export function Alert() {
+function NoConnect() {
+  return (
+    <div className="flex justify-center items-center py-2">
+      <AlertNoConnect />
+      <card.Card className="w-full max-w-2xl">
+        <card.CardTitle>
+          <card.CardHeader>
+            <card.CardTitle className="flex gap-2 justify-center items-center text-xl">
+              No Providers Connected
+            </card.CardTitle>
+          </card.CardHeader>
+        </card.CardTitle>
+      </card.Card>
+    </div>
+  )
+}
+
+function AlertNoConnect() {
   return (
     <AlertDialog defaultOpen>
-      <AlertDialogContent className="">
+      <AlertDialogContent className="sm:rounded-xl">
         <AlertDialogHeader>
           <AlertDialogTitle>No Providers Connected</AlertDialogTitle>
           <AlertDialogDescription>
