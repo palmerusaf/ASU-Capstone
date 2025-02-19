732ca4b36ba2157e376b44e2ab52edd5144465ed
Edin Ramovic <edinramovic4@gmail.com>
2025-02-19 15:12:17 -0600

    Added file for Local Storage Constants

---
+++ src/services/resume/resume.service.ts
@@ -0,0 +1,4 @@
+import resumeSchema from "resume-schema"
+import { storage } from "wxt/storage";
+import { RESUME_STORAGE_KEY } from "@/utils/constants/local.storage.constants.ts";
+import { ResumeRequest } from "@/src/models/resume.request.ts";
---
+++ utils/constants/local.storage.constants.ts
@@ -0,0 +1,4 @@
+export const JOB_STORAGE_KEY = "local:jobs";
+
+export const RESUME_STORAGE_KEY = "local:resumes";
+
