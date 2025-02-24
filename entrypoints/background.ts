98c1ec284973b526e38000921ade7ea949d6b29e
arya <aryaatarzadeh@gmail.com>
2025-02-25 00:13:40 +0800

    MVP1 of extention. Dyanically makes get request on behalf of user based on their selected job and prints the job information to console.

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -8,4 +8,34 @@ export default defineBackground(() => {
       active: true,
     });
   });
+
+  browser.runtime.onMessage.addListener(async function test(message, sender, sendResponse) {
+    if (message.type === 'Handshake-fetchJobData') {
+      const url = `${message.data.arg1.response}/search_preview`;
+      const res = await fetch(
+        // "https://asu.joinhandshake.com/stu/jobs/9694302/search_preview" 
+        url,
+        {
+          headers: {
+            accept: "application/json, text/javascript, */*; q=0.01",
+            priority: "u=1, i",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-requested-with": "XMLHttpRequest",
+          },
+          referrer: "https://asu.joinhandshake.com/stu/postings",
+          referrerPolicy: "strict-origin-when-cross-origin",
+          body: null,
+          method: "GET",
+          mode: "cors",
+          credentials: "include",
+        },
+      );
+      const json = await res.json();
+      console.log(json);
+      return { status: 'Job Saved!' };
+    }
+  }
+  );
 });
--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -10,8 +10,29 @@ function App() {
         });
     }
 
+    // Finds and returns the url of the selected job posting as simplified form in user's active (https://asu.joinhandshake.com/stu/postings) window using DOM.
     function saveJob() {
-        console.log("Test");
+        browser.tabs.query({ active: true, currentWindow: true })
+            .then(tabs => {
+                const activeTab = tabs[0];
+                if (activeTab?.id) {
+                    // Makes call to handshake.content.ts to get job URL.
+                    browser.tabs.sendMessage(activeTab.id, { message: "Handshake-getJobURL" })
+                        .then(url => {
+                            // Make call to background.ts to get job information.
+                            browser.runtime.sendMessage({
+                                type: "Handshake-fetchJobData",
+                                data: { arg1: url },
+                            })
+                                .then(response2 => {
+                                    // TODO: Save job data to database.
+                                });
+                        })
+                        .catch(error => {
+                            console.error("Error: ", error);
+                        });
+                }
+            });
     };
 
     return (
