5ed1c56039676ca692d05708ed15d2a114bc936e
Branden Palmer <brandenpalmer08@gmail.com>
2025-03-22 14:38:53 +0700

    fix storybook setup and add sidebar story

--- .storybook/vite.config.js
+++ .storybook/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '..'), // Ensure it points to the correct directory
--- components/layout.stories.tsx
+++ components/layout.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
+import * as icon from 'lucide-react';
 import { Layout } from './layout';
+import { JSX, useEffect, useState } from 'react';
 
 const meta = {
   component: Layout,
@@ -10,4 +11,60 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+const menuData = [
+  {
+    title: 'Manage Applications',
+    icon: icon.Briefcase,
+    items: [
+      {
+        title: 'View Jobs',
+      },
+    ],
+  },
+  {
+    title: 'Manage Resumes',
+    icon: icon.Pencil,
+    items: [
+      {
+        title: 'Upload Resume',
+      },
+    ],
+  },
+  {
+    title: 'Settings',
+    icon: icon.Settings2,
+    items: [
+      {
+        title: 'Display',
+      },
+    ],
+  },
+] as const;
+export type MenuData = typeof menuData;
+export type MainMenu = MenuData[number]['title'];
+export type SubMenu = MenuData[number]['items'][number]['title'];
+type PageRouterType = { [key in SubMenu]: React.ReactNode };
+
+const pageRouter: PageRouterType = {
+  Display: 'Display',
+  'Upload Resume': '',
+  'View Jobs': '',
+};
+export const Default: Story = {
+  render: () => {
+    const [active, setActive] = useState<{ menu: MainMenu; submenu: SubMenu }>({
+      menu: 'Settings',
+      submenu: 'Display',
+    });
+    return (
+      <Layout
+        setActive={setActive}
+        menuData={menuData}
+        menu={active.menu}
+        submenu={active.submenu}
+      >
+        {pageRouter[active.submenu] || 'not implemented'}
+      </Layout>
+    );
+  },
+};
