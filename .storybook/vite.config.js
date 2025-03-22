94d1bacc4096563e9d48ac6599b956fe3c0df1c6
Branden Palmer <brandenpalmer08@gmail.com>
2025-03-22 13:53:18 +0700

    fix import path for storybook

--- .storybook/vite.config.js
+++ .storybook/vite.config.js
@@ -1,3 +1,11 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
-export default defineConfig({});
+export default defineConfig({
+  plugins: [],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '..'), // Ensure it points to the correct directory
+    },
+  },
+});
---
+++ components/layout.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Layout } from './layout';
+
+const meta = {
+  component: Layout,
+} satisfies Meta<typeof Layout>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
