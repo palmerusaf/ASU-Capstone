4a8a804ce226a667cad8f2c593badc7c4e0c2049
Branden Palmer <brandenpalmer08@gmail.com>
2025-04-14 16:13:04 +0700

    implement google auth

--- utils/auth.ts
+++ utils/auth.ts
@@ -52,3 +52,44 @@ export async function signInWithGithub() {
     }
   );
 }
+
+export async function signInWithGoogle() {
+  const manifest = chrome.runtime.getManifest();
+
+  const url = new URL('https://accounts.google.com/o/oauth2/auth');
+
+  if (!manifest.oauth2 || !manifest.oauth2.scopes)
+    return console.error('Manifest misconfig for google auth');
+
+  url.searchParams.set('client_id', manifest.oauth2.client_id);
+  url.searchParams.set('response_type', 'id_token');
+  url.searchParams.set('access_type', 'offline');
+  url.searchParams.set(
+    'redirect_uri',
+    `https://${chrome.runtime.id}.chromiumapp.org`
+  );
+  url.searchParams.set('scope', manifest.oauth2.scopes.join(' '));
+
+  chrome.identity.launchWebAuthFlow(
+    {
+      url: url.href,
+      interactive: true,
+    },
+
+    async (redirectedTo) => {
+      if (chrome.runtime.lastError || !redirectedTo)
+        return console.error('Auth failed', chrome.runtime.lastError);
+
+      const token = new URLSearchParams(new URL(redirectedTo).hash).get(
+        '#id_token'
+      );
+
+      if (!token) return console.error('Google auth token null');
+
+      await supabase.auth.signInWithIdToken({
+        provider: 'google',
+        token: token,
+      });
+    }
+  );
+}
