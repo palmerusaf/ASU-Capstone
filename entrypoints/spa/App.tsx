0e1973badecb438df0b03fb4df588e50f60cef4a
arya <aryaatarzadeh@gmail.com>
2025-04-23 13:14:19 +0800

    misc

--- entrypoints/spa/App.tsx
+++ entrypoints/spa/App.tsx
@@ -1,7 +1,9 @@
 import { DisplaySettings } from '@/components/display-settings';
+import { JobTrackerPage } from '@/components/job-tracker-Page/page';
 import { LoginPage } from '@/components/login-page';
 import { QueryExample } from '@/components/query-example';
 import { QueryProvider } from '@/components/query-provider';
+import { ResumeDisplay } from '@/components/resume-display';
 import { ResumeUpload } from '@/components/resume-upload.tsx';
 import useAuth from '@/utils/auth';
 import * as icon from 'lucide-react';
