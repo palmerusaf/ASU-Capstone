f4f4cb3df322a580ae23b35db40f46de64374d78
Branden Palmer <brandenpalmer08@gmail.com>
2025-09-02 22:51:01 +0700

    format rows

--- components/job-tracker-Page/columns.tsx
+++ components/job-tracker-Page/columns.tsx
@@ -1,8 +1,24 @@
 // (client component) will contain our column definitions.
 import logo from '/wxt.svg';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
-import { HandshakeJobDataType, jobStatusEmojis } from '@/utils/db/schema';
+import {
+  HandshakeJobDataType,
+  jobStatusEmojis,
+  jobTable,
+} from '@/utils/db/schema';
 import { ColumnDef } from '@tanstack/react-table';
+import { Button } from '../ui/button';
+import { Label } from '@radix-ui/react-dropdown-menu';
 
 export const columns: ColumnDef<HandshakeJobDataType>[] = [
   {
@@ -10,7 +26,15 @@ export const columns: ColumnDef<HandshakeJobDataType>[] = [
     header: '',
     cell: ({ row }) => {
       const imgUrl = row.original.companyLogoUrl ?? logo;
-      return <img src={imgUrl} alt={imgUrl} width='25' height='25' />;
+      return (
+        <img
+          className='ml-2'
+          src={imgUrl}
+          alt={imgUrl}
+          width='25'
+          height='25'
+        />
+      );
     },
   },
   {
@@ -24,19 +48,24 @@ export const columns: ColumnDef<HandshakeJobDataType>[] = [
       const title = row.getValue('title') as string;
 
       return (
-        <a
-          href={row.original.link}
-          target='_blank'
-          rel='noopener noreferrer'
-          className='text-blue-700'
-        >
-          {title}
-        </a>
+        <div className='w-[16ch] overflow-hidden overflow-ellipsis text-blue-700'>
+          <a href={row.original.link} target='_blank' rel='noopener noreferrer'>
+            {title}
+          </a>
+        </div>
       );
     },
   },
   {
-    accessorKey: 'location',
+    cell: ({
+      row: {
+        original: { location },
+      },
+    }) => (
+      <div className='w-[16ch] overflow-hidden overflow-ellipsis'>
+        {location}
+      </div>
+    ),
     header: 'Location',
   },
   {
@@ -53,9 +82,10 @@ export const columns: ColumnDef<HandshakeJobDataType>[] = [
     }) =>
       payrate
         ? new Intl.NumberFormat('en-US', {
-          style: 'currency',
-          currency: 'USD',
-        }).format(payrate / 100)
+            style: 'currency',
+            currency: 'USD',
+            maximumFractionDigits: 0,
+          }).format(payrate / 100)
         : 'n/a',
   },
   {
@@ -67,4 +97,33 @@ export const columns: ColumnDef<HandshakeJobDataType>[] = [
       },
     }) => <span className='text-lg'>{jobStatusEmojis[status]}</span>,
   },
+  {
+    header: 'Details',
+    cell: ({ row: { original } }) => <JobModal data={original} />,
+  },
 ];
+
+function JobModal({ data }: { data: typeof jobTable.$inferSelect }) {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button className='p-3 py-1'>View</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Job Details</DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+        <div>
+          <Label>Title:</Label>
+          {data.title}
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant='outline'>Close</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
