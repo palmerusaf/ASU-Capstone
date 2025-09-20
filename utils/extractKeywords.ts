9ba9d7539a261085a799bf6e9c8583f6dcc6ff43
arya <aryaatarzadeh@gmail.com>
2025-09-20 16:16:55 +0800

    Added compute-cosine-similarity library

--- utils/extractKeywords.ts
+++ utils/extractKeywords.ts
@@ -1,3 +1,5 @@
+import Similarity from 'compute-cosine-similarity';
+
 const STOP = new Set([
   'a',
   'an',
@@ -47,4 +49,36 @@ export function getTopNKeywords({
     .sort((a, b) => b[1] - a[1])
     .slice(0, numKeywords)
     .map(([k]) => k);
+}
+
+export function calculateCosineSimilarity(
+  jobDescription: string,
+  resumeText: string
+) {
+  // Extract keywords
+  const jobKeywords = extractKeywords(jobDescription);
+  const resumeKeywords = extractKeywords(resumeText);
+
+  // Combine all keywords
+  const keywords = new Set([...jobKeywords.keys(), ...resumeKeywords.keys()]);
+
+  // Vector to be used to calculate cosine similarity score
+  const jobArray: number[] = [];
+  const resumeArray: number[] = [];
+
+  // Populate array with number of occurences of keyword in resume and job description
+  // Note: Number of occurences does not actually matter when calculating cosineSimilarity.
+  for (const word of keywords) {
+    jobArray.push(jobKeywords.get(word) ?? 0);
+    resumeArray.push(resumeKeywords.get(word) ?? 0);
+  }
+
+  // If an array is empty due to STOP words, return 0 instead. 
+  if (jobArray.every((occurance) => occurance === 0) || resumeArray.every((occurance) => occurance === 0)) {
+    return 0;
+  }
+
+  // Calculate similarity score
+  const similarity = Similarity(jobArray, resumeArray);
+  return similarity;
 }
