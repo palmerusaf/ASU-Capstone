ddc3f1bedad2c5c6225d020523586dbd6a7bdd85
Edin Ramovic <edinramovic4@gmail.com>
2025-10-14 14:14:22 -0500

    Removed local storage resume methods

--- utils/db/localStorage.ts
+++ utils/db/localStorage.ts
@@ -8,12 +8,3 @@ export async function getJobs() {
 export async function addJob(job: typeof jobs.$inferSelect) {
   await storage.setItem('local:jobs', [...(await getJobs()), job]);
 }
-
-export async function getResumes() {
-  return (await storage.getItem("local:resumes")) || [];
-}
-
-export async function addResume(resume: unknown) {
-  const resumes = await getResumes();
-  await storage.setItem("local:resumes", [...resumes, resume]);
-}
