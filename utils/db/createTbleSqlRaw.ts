8e82d68cb90d5b48a8ed1a1e2cd2d95cb769e9c0
Edin Ramovic <edinramovic4@gmail.com>
2025-10-14 14:12:43 -0500

    Added new resume table create statements

--- utils/db/createTbleSqlRaw.ts
+++ utils/db/createTbleSqlRaw.ts
@@ -23,7 +23,25 @@ CREATE TABLE "testSchema" (
 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "testSchema_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"test_field" text NOT NULL
 );
-
+--> statement-breakpoint
+CREATE TABLE "resumes" (
+                           "id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "resumes_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+                           "user_id" text,
+                           "name" text NOT NULL,
+                           "json" jsonb NOT NULL,
+                           "created_at" timestamp DEFAULT now() NOT NULL,
+                           "updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "raw_resumes" (
+                               "id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "raw_resumes_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+                               "name" text NOT NULL,
+                               "raw_text" text NOT NULL,
+                               "source" text NOT NULL,
+                               "json_id" integer,
+                               "created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE "job_comments" (
 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "job_comments_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"job_id" integer NOT NULL,
@@ -35,4 +53,5 @@ CREATE TABLE "job_comments" (
 ALTER TABLE "job_comments" ADD CONSTRAINT "job_comments_job_id_jobs_id_fk" FOREIGN KEY ("job_id") REFERENCES "public"."jobs"("id") ON DELETE cascade ON UPDATE no action;
 ALTER TABLE "jobs" ADD COLUMN "archived" boolean DEFAULT false NOT NULL;
 ALTER TABLE "jobs" ADD COLUMN "pay_type" text;
+ALTER TABLE "raw_resumes" ADD CONSTRAINT "raw_resumes_json_id_resumes_id_fk" FOREIGN KEY ("json_id") REFERENCES "public"."resumes"("id") ON DELETE set null ON UPDATE no action;
 `;
