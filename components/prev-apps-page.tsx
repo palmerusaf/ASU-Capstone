443e7bbe2ab7c495692e3b424904ac4cd0a107be
Branden Palmer <brandenpalmer08@gmail.com>
2025-10-17 21:16:51 +0700

    add ui

--- components/prev-apps-page.tsx
+++ components/prev-apps-page.tsx
@@ -1,20 +1,112 @@
 import { db } from '@/utils/db/db';
-import { jobEventsTable, jobTable } from '@/utils/db/schema';
+import { jobEventsTable, JobSelectType, jobTable } from '@/utils/db/schema';
 import { useQuery } from '@tanstack/react-query';
 import { eq } from 'drizzle-orm';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Card } from './ui/card';
+import { DataTable } from './job-archive-Page/data-table';
+import { ColumnDef } from '@tanstack/react-table';
 
 export function PrevAppsPage() {
+  // 🧩 Include createdAt column
+  const columns: ColumnDef<JobSelectType & { createdAt: Date }>[] = [
+    {
+      accessorKey: 'title',
+      header: 'Title',
+      cell: ({ row }) => {
+        const title = row.getValue('title') as string;
+        return (
+          <div className='overflow-hidden ml-4 overflow-ellipsis text-blue-700'>
+            <a
+              href={row.original.link}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              {title}
+            </a>
+          </div>
+        );
+      },
+    },
+    {
+      header: 'Location',
+      cell: ({ row: { original } }) => (
+        <div className='w-[16ch] overflow-hidden overflow-ellipsis'>
+          {original.location}
+        </div>
+      ),
+    },
+    {
+      accessorKey: 'employmentType',
+      header: 'Employment',
+    },
+    {
+      accessorKey: 'payrate',
+      header: 'Pay',
+      cell: ({ row: { original } }) => (
+        <PayRate payrate={original.payrate} payType={original.payType} />
+      ),
+    },
+    {
+      accessorKey: 'createdAt',
+      header: 'Applied On',
+      cell: ({ row }) => {
+        const createdAt = new Date(row.getValue('createdAt') as string);
+        return <span>{createdAt.toLocaleDateString()}</span>;
+      },
+    },
+    {
+      header: 'Details',
+      cell: ({ row: { original } }) => (
+        <JobModal label='View' data={original} />
+      ),
+    },
+  ];
+
   const { data } = useQuery({
     queryKey: ['prevApp'],
-    queryFn: () =>
-      db
+    queryFn: async () =>
+      await db
         .select()
         .from(jobEventsTable)
         .where(eq(jobEventsTable.eventType, 'applied'))
-        .fullJoin(jobTable, eq(jobTable.id, jobEventsTable.jobId)),
+        .innerJoin(jobTable, eq(jobTable.id, jobEventsTable.jobId)),
   });
-  const companies = new Set(data?.map((val) => val.jobs?.companyName));
-  // __AUTO_GENERATED_PRINT_VAR_START__
-  console.log('PrevAppsPage companies:', companies); // __AUTO_GENERATED_PRINT_VAR_END__
-  return 'prev app page';
+
+  if (!data) return 'Loading...';
+
+  // group by company
+  const companies = [...new Set(data.map((d) => d.jobs.companyName))];
+
+  return (
+    <Card className='m-12 p-6'>
+      {companies.length ? (
+        companies.map((company) => (
+          <Accordion key={company} type='single' collapsible>
+            <AccordionItem value={company}>
+              <AccordionTrigger>{company}</AccordionTrigger>
+              <AccordionContent>
+                <DataTable
+                  columns={columns as ColumnDef<any>[]}
+                  data={data
+                    .filter((el) => el.jobs.companyName === company)
+                    .map(({ jobs, job_events: { createdAt } }) => ({
+                      ...jobs,
+                      createdAt,
+                    }))}
+                />
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        ))
+      ) : (
+        <div className='text-center text-lg'>No Data</div>
+      )}
+    </Card>
+  );
 }
