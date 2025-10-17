8218bd4f16029e8dbb2c3ba56843cf6a4b352d4d
Branden Palmer <brandenpalmer08@gmail.com>
2025-10-17 20:26:16 +0700

    add fetch

--- components/prev-apps-page.tsx
+++ components/prev-apps-page.tsx
@@ -1,3 +1,20 @@
+import { db } from '@/utils/db/db';
+import { jobEventsTable, jobTable } from '@/utils/db/schema';
+import { useQuery } from '@tanstack/react-query';
+import { eq } from 'drizzle-orm';
+
 export function PrevAppsPage() {
+  const { data } = useQuery({
+    queryKey: ['prevApp'],
+    queryFn: () =>
+      db
+        .select()
+        .from(jobEventsTable)
+        .where(eq(jobEventsTable.eventType, 'applied'))
+        .fullJoin(jobTable, eq(jobTable.id, jobEventsTable.jobId)),
+  });
+  const companies = new Set(data?.map((val) => val.jobs?.companyName));
+  // __AUTO_GENERATED_PRINT_VAR_START__
+  console.log('PrevAppsPage companies:', companies); // __AUTO_GENERATED_PRINT_VAR_END__
   return 'prev app page';
 }
