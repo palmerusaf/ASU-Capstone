146dc51536c8c2d0ab5c9ef2aab77f78011bfd6e
Branden Palmer <brandenpalmer08@gmail.com>
2025-10-18 19:08:28 +0700

    add sankey

--- components/total-jobs-page.tsx
+++ components/total-jobs-page.tsx
@@ -1,3 +1,13 @@
+import Plot from 'react-plotly.js';
 export function TotalJobs() {
-  return 'total jobs stats page';
+  // TODO: fix styling for dark/light mode
+  return <Plot data={[{ type: 'sankey' }]} layout={{}} />;
+}
+
+function getSanKeyJobData() {
+  // get total jobs
+  // get total applied jobs
+  // break applied jobs down by last status
+  // get total unapplied jobs
+  // break unapplied jobs down by last status
 }
